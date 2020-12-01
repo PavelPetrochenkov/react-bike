2fe6200534c97a8d227004b86d33230fdf524bba
PavelPetrochenkov <48198766+PavelPetrochenkov@users.noreply.github.com>
2020-12-01 18:47:10 +0200

    heroku

--- src/components/MyBikes/MyBikeList.js
+++ src/components/MyBikes/MyBikeList.js
@@ -9,7 +9,7 @@ function MyBikeList({myBikeList,bikeList}){
         }
     }))
     let i= bikeListT.reduce((sum,item)=>{
-        let time = Math.ceil((new Date() - new Date(item.startTimeOfUse))/1000/60/60);
+        let time = Math.ceil(((new Date() - new Date(item.startTimeOfUse))+1000)/1000/60/60);
        return sum+=(item.bike)?((time>=20)?time/2:time)*item.bike.price:0
     },0)
 
@@ -17,7 +17,7 @@ function MyBikeList({myBikeList,bikeList}){
             <h3><span>&#129321;</span> Your rent (Total: ${i.toFixed(2)})</h3>
             { 
             (bikeListT.map((bike,index) =>{
-               return <MyBikeItem bike={bike.bike} time={Math.ceil((new Date() - new Date(bike.startTimeOfUse))/1000/60/60)} key={index} />
+               return <MyBikeItem bike={bike.bike} time={Math.ceil(((new Date() - new Date(bike.startTimeOfUse))+1000)/1000/60/60)} key={index} />
             }))}
         </div>
     
