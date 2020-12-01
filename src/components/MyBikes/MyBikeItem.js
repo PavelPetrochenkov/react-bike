162e725247805a766103aa041eaaf7ee805c6f01
PavelPetrochenkov <48198766+PavelPetrochenkov@users.noreply.github.com>
2020-12-01 10:44:30 +0200

    heroku

--- src/components/MyBikes/MyBikeItem.js
+++ src/components/MyBikes/MyBikeItem.js
@@ -5,7 +5,7 @@ function MyBikeItem({bike,time}){
     const {cancelBike} = useContext(Context)
     return(bike)?
         <div className='block-bike'>
-        <div><span className="block-text">{bike.name} / {bike.type} / ${bike.price} for each 1 hour / Rented time: {time} h / Pay: ${(time*bike.price).toFixed()} </span></div>
+        <div><span className="block-text">{bike.name} / {bike.type} / ${bike.price} for each 1 hour / Rented time: {time} h / To pay: ${(time*bike.price).toFixed()} </span></div>
         <div  className='block-buttons'>
         <div className="block button-block"><button className="button-delete" onClick={()=>cancelBike(bike._id)}>Cancel</button></div>
         </div>
